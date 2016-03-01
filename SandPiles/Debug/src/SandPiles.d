5aac3e1fef8a80ef31a4430344a296a259acbcf2
iAmTheSystem <simonreichel@yahoo.de>
2016-03-01 12:37:51 +0100

    added Folder Sandpiles

---
+++ SandPiles/Debug/src/SandPiles.d
@@ -0,0 +1,1 @@
+src/SandPiles.d: ../src/SandPiles.cpp
---
+++ SandPiles/src/SandPiles.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SandPiles.cpp
+// Author      : Simon Reichel and Johann Tomtschak
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Simmulating Sand Piles
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "! Sand Piles Start !" << endl; // prints !!!Hello World!!!
+	return 0;
+}
